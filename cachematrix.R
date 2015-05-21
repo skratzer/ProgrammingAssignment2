ca9aaba66d1e9d620770c5656c201cd0cb641bde
Sean Kratzer <s.kratzer@live.com>
2015-05-21 14:05:29 -0700

    Revert "submittal cachematrix.R"
    This reverts commit 3f08f1d089d1e4e75092fe5dc2c9da3b02a158cc.

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,15 @@
-makeCacheMatrix <- function(x=matrix()){
-  m<-NULL
-  set <- function(y){
-    x<<-y
-    m<<-NULL
-  }
-  get<-function() x
-  setmatrix<-function(solve) m<<-solve
-  getmatrix<-function() m
-  list(set=set,get=get,setmatrix=setmatrix,getmatrix=getmatrix)
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
-cacheSolve<-function(x=matrix(),...){
-  m<-x$getmatrix()
-  if(!is.null(m)){
-    message("getting cached data")
-    return(m)
-  }
-  
-  mat<-x$get()
-  m<-solve(mat,...)
-  x$setmatrix(m)
-  m
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
